3a9fe86f1eece71d7897d90bacd19a7b07c245ba
Tuan Tran <tuantran@Tuans-Mac-mini.local>
2021-10-21 09:04:46 +0700

    .

--- opium/app/Models/Post.php
+++ opium/app/Models/Post.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use App\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\ImageManagerStatic as Image;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
@@ -27,26 +30,26 @@ class Post extends Model
     {
         $attribute_name = "photo";
         $disk = "public";
-        $destination_path = "/images";
+        $destination_path = "opium/public/images";
 
         // if the image was erased
         if ($value==null) {
             // delete the image from disk
-            \Storage::disk($disk)->delete($this->{$attribute_name});
+            Storage::disk($disk)->delete($this->{$attribute_name});
 
             // set null in the database column
             $this->attributes[$attribute_name] = null;
         }
 
         // if a base64 was sent, store it in the db
-        if (starts_with($value, 'data:image'))
+        if (str_starts_with($value, 'data:image'))
         {
             // 0. Make the image
-            $image = \Image::make($value)->encode('jpg', 90);
+            $image = Image::make($value)->encode('jpg', 90);
             // 1. Generate a filename.
             $filename = md5($value.time()).'.jpg';
             // 2. Store the image on disk.
-            \Storage::disk($disk)->put($destination_path.'/'.$filename, $image->stream());
+            Storage::disk($disk)->put($destination_path.'/'.$filename, $image->stream());
             // 3. Save the path to the database
             $this->attributes[$attribute_name] = $destination_path.'/'.$filename;
         }
